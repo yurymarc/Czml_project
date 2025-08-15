9c1daec2fba1eddad846d642a8c758f1c9043b46
Paulo Cesar Coronado <paulocoronado@gmail.com>
2025-08-15 16:30:11 -0500

    v 0.2

---
+++ program.js
@@ -0,0 +1,17 @@
+async function init() {
+    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyYzZjNjkwNy1hNmJjLTQ3Y2EtOWQ5NS0yYzM4Mzk0YzkzNjciLCJpZCI6MzE4NTIsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1OTYwNjUyNzB9.RvMzC5gf0JKfZzhl3nX0g60_MqWICPLGfshdhNhscH4';
+
+    const viewer = new Cesium.Viewer('cesiumContainer', {
+        terrain: Cesium.Terrain.fromWorldTerrain(),
+    });
+
+    try {
+        const ds = await Cesium.CzmlDataSource.load('circulo_dinamico.czml');
+        viewer.dataSources.add(ds);
+        await viewer.zoomTo(ds);
+    } catch (e) {
+        console.error('Error cargando CZML:', e);
+    }
+}
+
+init();
